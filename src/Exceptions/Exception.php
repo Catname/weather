df33c2dc406c85fbd6e641cdfdb8c505fc816cee
ZhangHQ <tomcath@foxmail.com>
2021-09-16 14:57:10 +0800

    完成天气接口请求，增加异常及错误处理

---
+++ src/Exceptions/Exception.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author: ZhangHQ
+ * @email : tomcath@foxmail.com
+ */
+namespace Catname\Weather\Exceptions;
+
+class Exception extends \Exception
+{
+
+}
---
+++ src/Exceptions/HttpException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author: ZhangHQ
+ * @email : tomcath@foxmail.com
+ */
+
+namespace Catname\Weather\Exceptions;
+
+class HttpException extends Exception
+{
+
+}
